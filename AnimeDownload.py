18bd0007a1e8f22a48fced95df4cb25b51c23189
之雨商 <1299172402@qq.com>
2020-11-21 22:09:21 +0800

    Add files via upload

--- AnimeDownload.py
+++ AnimeDownload.py
@@ -4,10 +4,11 @@ import json
 
 md = "0"
 ss = "0"
+qn = "80"
 AnimeTitle = ""
+ua = ""
+cookies = ""
 
-ua = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
-cookies = "_uuid=xxx; buvid3=xxx; CURRENT_FNVAL=80; blackside_state=1; sid=5phw8t6f; bsource=search_bing; DedeUserID=498766638; DedeUserID__ckMd5=axxx; SESSDATA=xxx; bili_jct=e8340d45xxxxxx907; LIVE_BUVID=AUxxxxx822; CURRENT_QUALITY=80; rpdid=|(k|mmJlY||m0J'uY|lY~)kYk; PVID=3; bfe_id=1xxxxa6"
 headers = {
     "Origin": "https://www.bilibili.com",
     "Referer": f"https://www.bilibili.com/bangmumi/play/ss{ss}",
@@ -22,22 +23,60 @@ headers_cookies = {
 }
 
 epsinfo = []
-workpath = r"D:\HHT\哔哩哔哩动画\AnimeDownload"
-danmaku2ass_path = r"D:\HHT\coooooding\AnimationDownload\danmaku2ass.py"
+workpath = ""
+danmaku2ass_path = ""
+danmaku2ass_option = ""
 animation_path = r""
 
-qn = "80"
-
-aria2c_path = r"D:\HHT\coooooding\AnimationDownload\aria2c.exe"
+aria2c_path = r""
 currentdownload = "10" # 同时文件下载数
 split = "20" # 单任务线程数
 allocation = "falloc" # aria2c --help=file-allocation
 serverconnection = "16" # 服务器连接数
-cookies_path = r"D:\HHT\coooooding\AnimationDownload\bilibili.cookies"
+cookies_path = r""
+aria2c_ua = ""
 
 is_xml_download = "y"
 is_danmaku2ass = "y"
 is_admination_download = "y"
+
+epinfo_output = ""
+
+def init():
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "AnimeDownloadConfig.json"), "r", encoding="utf-8") as f:
+        config = json.loads(f.read())
+
+    global workpath
+    workpath = config["path"]["workpath"]
+
+    global md, qn, ua
+    # md = "0"
+    qn = config["animation"]["qn"]
+    ua = config["animation"]["ua"]
+
+    global allocation, aria2c_path, cookies_path, currentdownload, serverconnection, split, aria2c_ua
+    allocation = config["aria2c"]["allocation"]
+    aria2c_path = config["aria2c"]["aria2c_path"]
+    cookies_path = config["aria2c"]["cookies_path"]
+    currentdownload = config["aria2c"]["currentdownload"]
+    serverconnection = config["aria2c"]["serverconnection"]
+    split = config["aria2c"]["split"]
+    aria2c_ua = config["aria2c"]["aria2c_ua"]
+
+    global danmaku2ass_path, danmaku2ass_option
+    danmaku2ass_path = config["danmaku2ass"]["danmaku2ass_path"]
+    danmaku2ass_option = config["danmaku2ass"]["danmaku2ass_option"]
+
+    global is_xml_download, is_danmaku2ass, is_admination_download
+    is_xml_download = config["work"]["is_xml_download"]
+    is_danmaku2ass = config["work"]["is_danmaku2ass"]
+    is_admination_download = config["work"]["is_admination_download"]
+
+    global epinfo_output
+    epinfo_output = config["other"]["epinfo_output"]
+
+    with open(cookies_path, "r") as f:
+        cookies = f.read()
 
 def TitleRename(title):
     title = title.replace("/", " ")
@@ -100,7 +139,8 @@ def DanmakuDownload():
             print(f"[xml download SUCCEED] {xml}")
 
 def Danmaku2ass():
-    os.system(f"python {danmaku2ass_path} {animation_path}")
+    command = f"""{danmaku2ass_path} "{animation_path}" {danmaku2ass_option} """
+    os.system(command)
 
 def AnimationDownloadList():
     global epsinfo, ss
@@ -112,21 +152,25 @@ def AnimationDownloadList():
         url = f"https://api.bilibili.com/pgc/player/web/playurl?cid={cid}&qn={qn}&fourk=1&avid={aid}"
         res = json.loads(requests.get(url, headers=headers_cookies).text)
         if res["code"] != 0:
-            print(res)
+            print(f"[{blv}] {res}")
             continue
         downloadurl = res["result"]["durl"][0]["backup_url"][1]
         f.write(downloadurl + "\n")
         f.write(f""" out={blv} \n""")
 
 def AnimationDownload():
-    global ss, aria2c_path, currentdownload, split, allocation, serverconnection, cookies_path, ua
+    global ss, aria2c_path, currentdownload, split, allocation, serverconnection, cookies_path, aria2c_ua
     file = os.path.join(workpath, f"s{ss}.txt")
-    command = f"""{aria2c_path} --referer="https://www.bilibili.com/" --input-file="{file}" --file-allocation={allocation} --dir="{animation_path}" -c --max-concurrent-downloads={currentdownload} --split={split} --load-cookies={cookies_path} --max-connection-per-server={serverconnection}"""
+    command = f"""{aria2c_path} --referer="https://www.bilibili.com/" --input-file="{file}" --file-allocation={allocation} --dir="{animation_path}" -c --max-concurrent-downloads={currentdownload} --split={split} --load-cookies={cookies_path} --max-connection-per-server={serverconnection} --user-agent={aria2c_ua} """
     print(command)
     os.system(command)
 
 def main():
     print("Welcome to Bilibili Animation Download")
+    print("Creative By ZhiyuShang With LOVE")
+    print("[Referer](https://github.com/1299172402/AnimeDownload)")
+
+    init()
 
     global md
     md = input("md: ")
@@ -134,6 +178,9 @@ def main():
     GetEpInfo()
     for ep in epsinfo:
         print(ep)
+    if epinfo_output == "y":
+        with open(os.path.join(workpath, f"ss{ss}.json"), "w", encoding="utf-8") as f:
+            f.write(json.dumps(epsinfo))
 
     if is_xml_download == "y":
         DanmakuDownload()
